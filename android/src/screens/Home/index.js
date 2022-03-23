d65de5f6f9dc58c641db64f94f22f3450a1834f4
Lorena Mendes Barbosa <75842072+LorenaMendesB@users.noreply.github.com>
2022-03-23 17:04:17 -0300

    1.0.0.1

--- android/src/screens/Home/index.js
+++ android/src/screens/Home/index.js
@@ -1,18 +1,15 @@
 import { StatusBar, View, Text } from 'react-native';
 import React from 'react';
-
+import {styles} from './styles'
 export function Home() {
   return (
-    <View style={{
-      flex:1,
-      alignItems: 'center',
-      justifyContenet: 'center'
-    }}>
-      <Text style={{fontSize: 25}}> TELA TESTE!!! </Text>
+    <SefeAreaView style={
+      styles.container}>
+      <Text style={styles.text}> TELA TESTE!!! </Text>
     <StatusBar 
       barStyle='light-content'
       backgroundColor='#121015' 
     />
-  </View>
+  </SefeAreaView>
   );
 }
