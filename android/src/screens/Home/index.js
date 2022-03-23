aa514435dd4dcbeda46a9f1101f3b16c017dd629
Lorena Mendes Barbosa <75842072+LorenaMendesB@users.noreply.github.com>
2022-03-23 17:50:54 -0300

    1.0.0.3

--- android/src/screens/Home/index.js
+++ android/src/screens/Home/index.js
@@ -1,4 +1,4 @@
-import { StatusBar, View, Text, TextInput } from 'react-native';
+import { StatusBar, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import React from 'react';
 import {styles} from './styles'
 export function Home() {
@@ -6,11 +6,21 @@ export function Home() {
     <SefeAreaView style={
       styles.container}>
       <Text style={styles.text}> Ola, Lorena</Text>
-    <TextInput style={ styles.input}/>
-    <StatusBar 
-      barStyle='light-content'
-      backgroundColor='#121015' 
+    <TextInput style={ styles.input}
+    placeholder="Digite sua tarefa..."
+    placeholderTextColor="#555"
     />
+    {/* <StatusBar 
+      barStyle='light-content'
+      backgroundColor="#121015" 
+    /> */}
+  <TouchableOpacity style={styles.button}>
+    <Text style ={styles.buttonText}>
+      Adicionar
+    </Text>
+
+
+  </TouchableOpacity>
   </SefeAreaView>
   );
 }
--- android/src/screens/Home/style.js
+++ android/src/screens/Home/style.js
@@ -1,4 +1,4 @@
-import { StyleSheet, TextInput } from "react-native";
+import { Platform, StyleSheet, TextInput } from "react-native";
 export const style = StyleSheet.create({
     container: {
         flex: 1,
@@ -15,7 +15,23 @@ export const style = StyleSheet.create({
         backgroundColor: '#1f1e25',
         color: 'white',
         fontSize: 18,
-        padding: 15,
-        marginHorizontal: 20
+        padding: Platform.OS === 'ios' ? 15 : 10,
+        marginHorizontal: 20,
     },
-})  
+    button:{
+        backgroundColor: 'green',
+        padding: 15,
+        margin: 20,
+        alignItems: 'center',
+        borderRadius: 7,
+        
+
+    },
+    buttonText:{
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+    })  
+    //Com o Platform é possível testar qual sistema operacionar para 
+    // fazer configurações
